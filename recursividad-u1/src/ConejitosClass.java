e55ee5e46e0bec9ab1c5df18ae4e01412edcda3a
JesusLeos16 <jelm060716@gmail.com>
2026-02-03 19:59:08 -0600

    Sube ConejitosClass hardcodeada para practicar

--- recursividad-u1/src/ConejitosClass.java
+++ recursividad-u1/src/ConejitosClass.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class ConejitosClass {
+    public void conejitos(int numero){
+        int orejas = numero*3;
+        if (numero==3){
+            orejas = orejas-3;
+            IO.println("Ya fueron todos los conejitos");
+            IO.println("Orejas de conejos radioactivos (de tres orejas): "+orejas);
+            return;
+        }
+
+        int resultado;
+        resultado = (2*numero);
+        IO.println("Conejito "+resultado +" suma "+orejas+" orejas a la cuenta");
+        conejitos(numero+1);
+
+    }
+
 }
--- recursividad-u1/src/Main.java
+++ recursividad-u1/src/Main.java
@@ -1,5 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    IO.println("Haciendo el primer commit");
+    IO.println("En la fila de los conejitos pares se formo: ");
+    ConejitosClass conejo = new ConejitosClass();
+    conejo.conejitos(1);
+
 }
